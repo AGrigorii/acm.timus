55a4ef3579406012a964cae88e0be13cf2402497
Grigorii <79806574742@ya.ru>
2018-10-21 23:35:34 +0300

    project template

---
+++ TestProject/TestProject/main.cpp
@@ -0,0 +1,51 @@
+#pragma comment(linker, "/STACK:64000000")
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <math.h>
+#include <algorithm>
+#include <string.h>
+#include <ctype.h>
+#include <iomanip>
+#include <iterator> 
+#include <unordered_map>
+#include <unordered_set>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string> 
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<ll, ll> pll;
+typedef pair<ull, ull> pull;
+
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define PI  3.14159265358979323846
+#define ACCEPTED return 0;
+#define name ""
+
+const int  maxlen = (int)(1e5) + 10;
+const int    base = (int)(1e9);
+const ll      mod = 1000 * 1000 * 1000 + 7;
+const ll   bigmod = 99194853094755497ll;
+const ll infinity = (ll) 1e18;
+
+int main()
+{
+#ifdef _DEBUG
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+#endif
+
+    ACCEPTED
+}
